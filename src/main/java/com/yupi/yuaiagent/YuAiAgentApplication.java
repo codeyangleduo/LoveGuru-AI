8556588e5e25f189040077cf866043ec165df4e0
eryi <1204372934@qq.com>
2025-07-31 16:45:30 +0800

    Init

---
+++ src/main/java/com/yupi/yuaiagent/YuAiAgentApplication.java
@@ -0,0 +1,19 @@
+package com.yupi.yuaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.print.DocFlavor;
+
+@SpringBootApplication
+public class YuAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YuAiAgentApplication.class, args);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
