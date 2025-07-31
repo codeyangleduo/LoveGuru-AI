4a519eff82dbef492c630a2b857dd239c29c46f0
eryi <1204372934@qq.com>
2025-07-31 17:03:36 +0800

    Init

---
+++ src/main/java/com/yupi/loveguruai/LoveGuruAiApplication.java
@@ -0,0 +1,13 @@
+package com.yupi.loveguruai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoveGuruAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoveGuruAiApplication.class, args);
+    }
+
+}
